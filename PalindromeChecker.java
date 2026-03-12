aad9433526cfe290323981a558bcae56be9e8802
pixelparker-web <rr7137@srmist.edu.in>
2026-03-12 15:14:56 +0530

    PalindromeChecker

--- PalindromeChecker.java
+++
@@ -1,7 +0,0 @@
-public class PalindromeChecker {
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App");
-    }
-    System.out.println("Please enter a string to check if it's a palindrome:");
-}
